8a221adcadeed48a0db8e2a23adf0942b18d43bc
Daniel Xie <dyx2@cornell.edu>
2024-03-24 19:41:41 -0400

    Added create_note and try_hit; wrote implementations
    - Added create_note and try_hit documentation and signatures
    - Implemented all functions in note.ml

--- lib/note.ml
+++ lib/note.ml
@@ -4,10 +4,34 @@ type accuracy =
   | Good
   | Miss
 
-(* [time] is the time that the note reaches the bar (i.e., the optimal time for
-   the player to hit it). [base_score] is the default score value of this note,
-   without combo applied. *)
 type t = {
   time : int;
   base_score : int;
+  has_been_hit : bool;
 }
+
+(* [miss_window] is how much leeway the player has before the note counts as a
+   miss (either early or late). *)
+let miss_window = 600
+let create_note time base_score = { time; base_score; has_been_hit = false }
+
+let try_hit note time_hit =
+  if note.has_been_hit then false
+  else if abs (time_hit - note.time) <= 600 then true
+  else false
+
+let calc_score note combo accuracy =
+  note.base_score * combo
+  *
+  match accuracy with
+  | Perfect -> 3
+  | Great -> 2
+  | Good -> 1
+  | Miss -> 0
+
+let calc_accuracy note time_hit =
+  let time_diff = abs (time_hit - note.time) in
+  if time_diff < 200 then Perfect
+  else if time_diff < 400 then Great
+  else if time_diff < 600 then Good
+  else Miss
