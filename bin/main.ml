8fb7679df90a07193c24fc79197deb292c003497
Daniel Xie <151884754+xie-daniel@users.noreply.github.com>
2024-04-09 21:14:52 -0400

    Accuracy, combo, and scoring rework (#13)
    * Implemented combo counter
    
    - Implemented combo counter display
    - Increment counter at each valid press
    
    * Notes can only award 1 combo, and disappear upon hit
    
    - Notes disappear upon being hit, and reappear when they respawn
    - Bug fix: only 1 combo should be awarded per note hit
    
    * Implemented combo breaking and stratified accuracy
    
    - Implemented logic for missing a note and losing your combo
    - Changed scoring system so that accuracy and points awarded are discrete instead of continuous
    - Added combo multiplier to scoring system

--- bin/main.ml
+++ bin/main.ml
@@ -3,6 +3,7 @@ open Finalproject
 let width = 1280
 let height = 720
 let score = ref 0
+let combo = ref 0
 let valid_press = ref true
 
 (* delete later; here to confirm button presses work *)
@@ -38,11 +39,17 @@ let setup () =
 
   (false, notes, buttons, music)
 
+let check_combo_break (note, break_combo) =
+  if break_combo then combo := 0;
+  note
+
 let draw notes buttons =
   let open Raylib in
-  let updated_notes = List.map Note.update notes in
+  let updated_notes = List.map check_combo_break (List.map Note.update notes) in
   let _ =
-    List.map (fun note -> draw_rectangle_rec note Color.black) updated_notes
+    List.map
+      (fun note -> draw_rectangle_rec (Note.get_sprite note) Color.black)
+      updated_notes
   in
   ();
 
@@ -52,12 +59,18 @@ let draw notes buttons =
   let handle_key_press note button key =
     (if is_key_down key then
        let () = draw_rectangle_rec button Color.red in
-       if !valid_press then
-         let collision = get_collision_rec note button in
-         let points = collision |> Rectangle.width |> floor |> int_of_float in
-         begin
-           score := !score + points;
-           if points = 0 then valid_press := false
+       if !valid_press && not (Note.has_been_hit note) then
+         let collision = get_collision_rec (Note.get_sprite note) button in
+         let overlap =
+           (collision |> Rectangle.height)
+           /. (note |> Note.get_sprite |> Rectangle.height)
+         in
+         if overlap = 0. then valid_press := false
+         else begin
+           score :=
+             !score + (overlap |> Note.calc_accuracy |> Note.calc_score !combo);
+           combo := !combo + 1;
+           Note.hit note
          end);
     if is_key_released key then begin
       valid_press := true;
@@ -91,6 +104,15 @@ let draw notes buttons =
     50 40 Color.black;
   draw_fps 5 5
 
+let draw_combo combo =
+  let open Raylib in
+  draw_text
+    (string_of_int !combo ^ "x")
+    (get_screen_width () - 125)
+    (get_screen_height () - 100)
+    40
+    (if !combo = 0 then Color.red else Color.black)
+
 let rec loop (pause, notes, buttons, music) =
   match Raylib.window_should_close () with
   | true -> Raylib.close_window ()
@@ -104,6 +126,8 @@ let rec loop (pause, notes, buttons, music) =
       clear_background Color.raywhite;
 
       draw notes buttons;
+
+      draw_combo combo;
 
       end_drawing ();
 
