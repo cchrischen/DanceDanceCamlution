78d2592b1d0805d5dfc7ea2df579b4392d4aa084
Nam Anh Dang <nd433@cornell.edu>
2024-04-28 17:55:03 -0400

    added python files and changed the docs

--- lib/beatmap.mli
+++ lib/beatmap.mli
@@ -15,7 +15,9 @@ module Song : sig
   val init : string -> song
   (**[init song_path] is the [song] created from the [song_path], including the
      [Raylib.Music.t] music object and beatmap. Also initializes the audio
-     device and starts the playing of the song in [song_path].*)
+     device and starts the playing of the song in [song_path]. Note that
+     currently, [init song_path] requires the beatmap file and the mp3 file to
+     be in the same directory. *)
 
   val is_on_next_note : song -> float -> bool
   (**[is_on_next_note s offset] checks whether the current time in the song
