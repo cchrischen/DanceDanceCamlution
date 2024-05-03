0d903937ad71e2e8ced7426e8405abdb65f8a375
Chris Chen <cc2785@cornell.edu>
2024-05-03 01:13:21 -0400

    add draw score

--- states/playState.ml
+++ states/playState.ml
@@ -135,4 +135,10 @@ let render () =
        (fun note ->
          draw_rectangle_rec (Note.get_sprite note) Constants.note_color)
        notes);
-  ignore (Utils.map3 handle_key_press notes buttons Constants.bindings)
+  ignore (Utils.map3 handle_key_press notes buttons Constants.bindings);
+  draw_text
+    ("Score: " ^ string_of_int !score)
+    ((get_screen_width () * 17 / 20)
+    - (String.length (string_of_int !score) * (get_screen_width () / 200)))
+    20 30 Color.lightgray;
+  draw_fps 5 5
