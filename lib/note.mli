60900313f7c528394b08c527e8e99c0470746e07
Shub500 <143766726+Shub500@users.noreply.github.com>
2024-05-05 15:51:11 -0400

    Update note.mli

--- lib/note.mli
+++ lib/note.mli
@@ -15,10 +15,23 @@ val create_note : float -> float -> t
     score value of this note without combo applied. *)
 
 val update : t -> bool
+(** [update note] updates [note] position based on the position of the note on
+    the window (i.e if a note reaches end of window height resets position of
+    the note and resets the hit status for note - otherwise it moves the note
+    down in the window based on the speed of the note) *)
+
 val hit : t -> unit
+(** [hit note] if note has been hit then we reset the note at the top of the
+    window, otherwise changes field of the note to be hit *)
+
 val get_sprite : t -> Rectangle.t
+(** [get_sprite note] returns the sprite field of the note *)
+
 val has_been_hit : t -> bool
+(** [has_been_hit note] returns the whether the note has been hit or not *)
+
 val get_speed : t -> float
+(** [get_speed note] returns the speed of the note *)
 
 val calc_score : int -> accuracy -> int
 (** Calculates the score for this note, given [combo] and [accuracy]. *)
